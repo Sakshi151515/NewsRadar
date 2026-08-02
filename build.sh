0a19276e52e9844859f28e5f529da8d0ef99cf24
Sakshi151515 <sakshiamrutkar15@gmail.com>
2026-08-02 11:58:13 +0530

    Create Render admin user

--- build.sh
+++ build.sh
@@ -5,3 +5,22 @@ pip install -r requirements.txt
 python manage.py collectstatic --noinput
 
 python manage.py migrate
+
+python manage.py shell -c "
+from django.contrib.auth import get_user_model
+User = get_user_model()
+
+username = 'admin'
+email = 'admin@gmail.com'
+password = 'Admin@123'
+
+if not User.objects.filter(username=username).exists():
+    User.objects.create_superuser(
+        username=username,
+        email=email,
+        password=password
+    )
+    print('Admin user created successfully')
+else:
+    print('Admin user already exists')
+"
